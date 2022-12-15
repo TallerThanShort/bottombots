615ef31818764bbd9e54d8a6de3236b768851dfc
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-12-15 13:52:54 +0100

    Ads update pt1

--- script.js
+++ script.js
@@ -41,3 +41,28 @@ window.onload = () => {
         document.getElementById('navbarthings').innerHTML += `<span class="link-1naFFL link-2eyudH" onclick="document.location.href = 'https://discord.com/api/oauth2/authorize?client_id=914396590901452820&redirect_uri=http%3A%2F%2Fbottombots.xyz%2Fauth&response_type=token&scope=identify%20email&state=${localStorage.getItem('oauth-state')}';">Log In</span>`
     }
 };
+
+const randomiseData = (data) => {
+    let tutal = 0;
+    let list = [];
+    data.forEach(doc => {
+        tutal++
+        list[tutal] = `${doc.data().id}`;
+    });
+    shuffle(list);
+    setTimeout(function(){setupSite(list)}, 100);
+}
+
+function shuffle(array) {
+    var m = array.length, t, i;
+    
+    while(m){
+        i = Math.floor(Math.random() * m--);
+
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+}
